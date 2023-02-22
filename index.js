ce0b0fd0e7f93d85a123fd6bb823febd46ab189d
retelibray <123306208+retelibray@users.noreply.github.com>
2023-02-22 18:46:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.write('hiiii mi nobre ewksdlflsdfs');
     res.write(`hola amigo: ${req.url}`)
+    
     console.log(`Just got a request at ${req.url}!`)
     res.end();
 }).listen(process.env.PORT || 3000);
