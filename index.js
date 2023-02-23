dcab58dab5f2f36f4d32faa20f69f9439999fb54
retelibray <123306208+retelibray@users.noreply.github.com>
2023-02-23 20:43:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.write('hiiii mi nobre ewksdlflsdfs');
     res.write(`hola amigo: ${req.url}`);
+    var c = req.url;
+    if (c == '/u'){
+        res.write('hi')
+    }
     res.end();
 }).listen(process.env.PORT || 3000);
